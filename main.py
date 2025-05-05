0d4335e582c97b4116f4a9ad2c53a1821f01b934
Vaheed <vivi128800@gmail.com>
2025-05-05 23:36:53 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+TELEGRAM_TOKEN = '7966826952:AAFXki6HhpWAaryvtBjVHUie4ao2R5vw-Nk'
+CHAT_ID = '5813349732'
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.json
+    message = data.get("message", "سیگنال جدید از TradingView دریافت شد!")
+    
+    send_text = f"📡 سیگنال جدید:\n\n{message}"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': send_text
+    }
+
+    r = requests.post(url, json=payload)
+    return "ok", 200
+
+if __name__ == '__main__':
+    app.run()
